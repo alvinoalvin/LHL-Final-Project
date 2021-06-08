4f36fd3e5a78cd7182ce781a22e3f26a383ac7da
alvinoalvin <alvinoalvin@gmail.com>
2021-06-07 18:51:45 -0700

    changed api tasks

--- express-back-end/routes/deliverables.js
+++ express-back-end/routes/deliverables.js
@@ -12,7 +12,7 @@ module.exports = db => {
     });
   });
 
-  router.get("/tasks", (request, response) => {
+  router.get("/tasks/", (request, response) => {
     db.query(
       `
       SELECT *, deliverables.id as id, (users.first_name ||' ' || users.last_name) as full_name
@@ -20,8 +20,23 @@ module.exports = db => {
       inner JOIN type on type_id = type.id
       inner JOIN status on status_id = status.id
       inner JOIN users on creator = users.id
-      where type.type = 'Task' and creator = 1
+      where type.type = 'Task'
       `
+    ).then(({ rows: deliverables }) => {
+      response.json(deliverables);
+    });
+  });
+
+  router.get("/tasks/:id", (request, response) => {
+    db.query(
+      `
+      SELECT *, deliverables.id as id, (users.first_name ||' ' || users.last_name) as full_name
+      FROM deliverables
+      inner JOIN type on type_id = type.id
+      inner JOIN status on status_id = status.id
+      inner JOIN users on creator = users.id
+      where type.type = 'Task' and creator = $1
+      `, [request.params.id]
     ).then(({ rows: deliverables }) => {
       response.json(deliverables);
     });
