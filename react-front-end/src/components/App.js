65189f482b4d4e3c6faa1d62db5a28f7c93417aa
zeamaria <lingardzea@gmail.com>
2021-06-11 12:41:17 -0700

    Remove unused internal notes

--- react-front-end/src/components/App.js
+++ react-front-end/src/components/App.js
@@ -177,38 +177,3 @@ export default function Application() {
   );
 }
 
-
-/* Template data */
-// class App extends Component {
-//   constructor(props) {
-//     super(props)
-//     this.state = {
-//       message: 'Click the button to load data!'
-//     }
-//   }
-
-//   fetchData = () => {
-//     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-//     .then((response) => {
-//       // handle success
-//       console.log(response.data) // The entire response from the Rails API
-
-//       console.log(response.data.message) // Just the message
-//       this.setState({
-//         message: response.data.message
-//       });
-//     }) 
-//   }
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <h1>{ this.state.message }</h1>
-//         <button onClick={this.fetchData} >
-//           Fetch Data
-//         </button>        
-//       </div>
-//     );
-//   }
-// }
-// export default App;
