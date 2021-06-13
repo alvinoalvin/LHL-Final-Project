429dc06665bde4a2305abc1672d799694c91691a
alvinoalvin <alvinoalvin@gmail.com>
2021-06-12 20:02:58 -0700

    working notes component

--- react-front-end/src/components/SkillDashboard/Notes.js
+++ react-front-end/src/components/SkillDashboard/Notes.js
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardActions, CardContent, Button, Input, Typography } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles({
   root: {
@@ -25,33 +27,54 @@ const axios = require('axios');
 export default function NotesList(props) {
   const classes = useStyles();
   const [notes, setNotes] = useState("");
-  const bull = <span className={classes.bullet}>• &nbsp;</span>;
+  const inputRef = useRef("");
 
   useEffect(() => {
     axios.get(`/api/notes/${props.userID}/${props.skillID}`)
       .then(response => {
         const data = response.data.map(note => {
-          return <div>{bull}{note.note}</div>
+          note.key = note.id
+          return note
         })
         setNotes(data);
-        console.log(data)
+        console.log("Notes:", data)
       })
       .catch(error => console.log(error));
-  }, []);
+  }, [notes]);
 
-  function addNote(note) {
+  function addNote() {
+    console.log("button called")
+    console.log(inputRef.current.lastChild.value)
     const newNote = {
       user_id: props.userID,
       skill_id: props.skillID,
-      note: note,
-
+      note: inputRef.current.lastChild.value,
     }
 
     return axios.post(`/api/notes`, newNote)
       .then(function(response) {
-        newNote.id = response.data.result.id
-        const notesCopy = [...props.notes, newNote]
-        props.setNote(notesCopy)
+        const notesCopy = notes
+        notesCopy.push(newNote)
+        console.log(notesCopy)
+        setNotes(notesCopy)
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+
+
+  function deleteTask(id) {
+    return axios.delete(`api/notes/${id}`, { id })
+      .then(function(response) {
+        console.log("bloop")
+        const noteCopy = notes.filter((note) => {
+          if (note.id !== id) {
+            note.key = note.id
+            return note
+          }
+        });
+        setNotes(noteCopy);
       })
       .catch(function(error) {
         console.log(error);
@@ -65,12 +88,28 @@ export default function NotesList(props) {
           Notes
           </Typography>
         <Typography className={classes.pos} >
-          {notes}
+          {Object.keys(notes).map((id) => {
+            return (<>
+              <div>
+                <IconButton
+                  aria-label="delete"
+                  onClick={(event) => {
+                    if (window.confirm('Are you sure you want to delete?')) {
+                      deleteTask(notes[id].id);
+                    }
+                  }}>
+                  <CloseIcon />
+                </IconButton>
+                {notes[id].note}
+              </div>
+            </>
+            )
+          })}
         </Typography>
       </CardContent>
       <CardActions>
-        <Input />
-        <Button size="small">add Note</Button>
+        <Input ref={inputRef}/>
+        <Button size="small" onClick={() => { addNote()}}>add Note</Button>
       </CardActions>
     </Card>
   );
