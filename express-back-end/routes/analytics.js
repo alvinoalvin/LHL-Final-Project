43742fee53966e1602e17856321d484bac82ddda
alvinoalvin <alvinoalvin@gmail.com>
2021-06-14 13:05:55 -0700

    end points for topSKills and time estimates

--- express-back-end/routes/analytics.js
+++ express-back-end/routes/analytics.js
@@ -43,5 +43,51 @@ module.exports = (db) => {
     });
   });
 
+  router.get("/analytics/topskills", (request, response) => {
+    console.log(request.body)
+    db.query(
+      `
+      select skills.name, skills.id,
+      count(*) filter(where status.id=1) as stage_count,
+      count(*) filter(where status.id=2) as progress_count,
+      count(*) filter(where status.id=3) as complete_count
+      from deliverables
+      JOIN status on status_id = status.id
+      JOIN users on assigned_to = users.id
+      JOIN skills on skill_id = skills.id
+      where type_id = 1 and assigned_to = $1 and deleted = false
+      group by  skills.name,skills.id
+
+      `, [request.body.id]
+    ).then(({ rows: skills }) => {
+      response.json(skills);
+    });
+  });
+
+  router.get("/analytics/topskills/timeest", (request, response) => {
+    const arr = JSON.parse(request.query.array);
+    let paramStr = "(";
+    for (let i = 1; i <= arr.length; i++) {
+      if (i != arr.length) {
+        paramStr += `$${i},`;
+      } else {
+        paramStr += `$${i}`;
+      }
+    }
+    paramStr += ")";
+
+    db.query(
+      `
+      select skill_id, status, sum(time_estimate_minutes) as estimated_time
+      from deliverables
+      join status on status_id = status.id
+      where skill_id in $1 group by status,skill_id
+      order by estimated_time desc
+      `, [paramStr]
+    ).then(({ rows: skills }) => {
+      response.json(skills);
+    });
+  });
+
   return router;
 };
